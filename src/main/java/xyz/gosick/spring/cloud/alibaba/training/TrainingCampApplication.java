1622716db6cb72a8dcf748bb385448ca251fd91b
kotori <kotori@kotoris-Mac-mini.local>
2020-09-24 14:35:25 +0800

    git init

---
+++ src/main/java/xyz/gosick/spring/cloud/alibaba/training/TrainingCampApplication.java
@@ -0,0 +1,13 @@
+package xyz.gosick.spring.cloud.alibaba.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/gosick/spring/cloud/alibaba/training/TrainingCampApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.gosick.spring.cloud.alibaba.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
